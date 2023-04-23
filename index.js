e8b9dc177225ee29ea3b2b1af933104dcb3042ae
Sylvia Mumbi <mumbi099@gmail.com>
2023-04-23 08:44:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+//Task1
+var customerName = "bob";
+
+//Task2
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+//Task3
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+//Task4
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = "Tina";
+    leastFavoriteCustomer = "Janet"
+}
